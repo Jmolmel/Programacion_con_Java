be0b352809026aae6db94ecc1c57d09969062eb5
Jmolmel <josemolinamelendez@gmail.com>
2023-10-29 12:39:07 +0100

    Ejercicio01

---
+++ 03_Lectura_de_datos_desde_teclado/ejercicios/T03Ejercicio01.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * Realiza un programa que pida dos números y que luego muestre el resultado
+ *de su multiplicación.
+ * 
+ * @author Jose Molina Melendez
+ */
+public class T03Ejercicio01 {
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Introduzca un numero: \n");
+    int num1 = sc.nextInt();
+
+    System.out.print("Introduzca otro numero: \n");
+    int num2 = sc.nextInt();
+
+    System.out.println("La numa de los numeros es: " + (num1+num2));
+  }
+}
