21c5ad12754ddb8d0e926265bd35d7b0e48271e8
Jmolmel <josemolinamelendez@gmail.com>
2023-10-30 11:34:36 +0100

    Corrección T02

--- 02_Variables/ejercicios/T02Ejercicio02.java
+++ 02_Variables/ejercicios/T02Ejercicio02.java
@@ -1,7 +1,7 @@
 /**
  * Crea la variable nombre y asígnale tu nombre completo. Muestra su valor por
  * pantalla de tal forma que el resultado del programa sea el mismo que en el
- * ejercicio 1 del capítulo 1.
+ * ejercicio 1 del capítulo 1
  * 
  * @author Jose Molina Melendez
  */
