6964906a0d4bbc752c7311d038bd11aa23e6634f
Jmolmel <josemolinamelendez@gmail.com>
2023-10-30 11:45:22 +0100

    Corrección T02

--- 02_Variables/ejercicios/T02Ejercicio05.java
+++ 02_Variables/ejercicios/T02Ejercicio05.java
@@ -1,6 +1,6 @@
 /**
  * Realiza un conversor de pesetas a euros. La cantidad en pesetas que se quiere
- * convertir deberá estar almacenada en una variable
+ * convertir deberá estar almacenada en una variable.
  * 
  * @author Jose Molina Melendez
  */
