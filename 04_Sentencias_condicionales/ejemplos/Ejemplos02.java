1a2e4dbff627c14d380aa3d8eaf49c512dca7024
Jmolmel <josemolinamelendez@gmail.com>
2023-10-31 16:04:47 +0100

    T04

---
+++ 04_Sentencias_condicionales/ejemplos/Ejemplos02.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * Explica tu codigo aqui
+ * 
+ * @author Jose Molina Melendez
+ */
+public class Ejemplos02 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner (System.in);
+    int numero;
+
+    System.out.print("Introduzca un numero que este entre el 1 y 100: ");
+    numero = sc.nextInt();
+    
+
+    if ((numero >= 1) && (numero <= 100)) {
+      System.out.println("Estas entre el 1 y el 100");
+    } else {
+      System.out.println("El numero no es correcto");
+    }
+  }
+  
+}
