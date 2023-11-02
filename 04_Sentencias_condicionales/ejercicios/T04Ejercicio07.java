185ba23c61b02263de7cfe96b9da3e4450e6b67a
Jmolmel <josemolinamelendez@gmail.com>
2023-11-02 13:24:44 +0100

    T04Ej08

--- 04_Sentencias_condicionales/ejercicios/T04Ejercicio07.java
+++ 04_Sentencias_condicionales/ejercicios/T04Ejercicio07.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 /**
- * Explica tu codigo aqui
+ * Realiza un programa que calcule la media de tres notas.
  * 
  * @author Jose Molina Melendez
  */
@@ -9,7 +9,26 @@ public class T04Ejercicio07 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    
+    Double nota1;
+    Double nota2;
+    Double nota3;
+    Double media;
+
+    System.out.println("Introduzca 3 notas para calcular su media.");   //duda como introducir un 
+    System.out.print("Introduzca la primera nota: ");
+    nota1 = sc.nextDouble();
+    System.out.print("Introduzca la segunda nota: ");
+    nota2 = sc.nextDouble();
+    System.out.print("Introduzca la tercera nota: ");
+    nota3 = sc.nextDouble();
+
+    media = ((nota1+nota2+nota3)/3);
+
+    if (media >= 5) {
+      System.out.printf("Estas aprobado tu nota media es:  %.2f", media);  
+    } else{
+      System.out.printf("Lamentablemente has suspendido, tu nota media es:  %.2f",media);
+    }
+    sc.close();
   }
-  
 }
---
+++ 04_Sentencias_condicionales/ejercicios/T04Ejercicio08.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+/**
+ * Amplía el programa anterior para que diga la nota del boletín (insuficiente,
+ * suficiente, bien, notable o sobresaliente).
+ * 
+ * @author Jose Molina Melendez
+ */
+public class T04Ejercicio08 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    Double nota1;
+    Double nota2;
+    Double nota3;
+    Double media;
+
+    System.out.println("Introduzca 3 notas para calcular su media.");   //duda como introducir un 
+    System.out.print("Introduzca la primera nota: ");
+    nota1 = sc.nextDouble();
+    System.out.print("Introduzca la segunda nota: ");
+    nota2 = sc.nextDouble();
+    System.out.print("Introduzca la tercera nota: ");
+    nota3 = sc.nextDouble();
+
+    media = ((nota1+nota2+nota3)/3);
+
+    if (media <5) {
+      System.out.printf("INSUFICIENTE tu nota media es:  %.2f", media);  
+    } 
+    if (media == 5) {
+      System.out.printf("SUFICIENTE tu nota media es:   %.2f", media);
+    }
+    if ((media >5) && (media <=6)) {
+      System.out.printf("BIEN tu nota media es:   %.2f", media);
+    }
+    if ((media >6) && (media <=7.5)) {
+      System.out.printf("NOTABLE! tu nota media es:   %.2f", media);
+    }
+    if ((media >7.5) && (media <= 10)) {
+      System.out.printf("SOBRESALIENTE! tu nota media es:   %.2f", media);
+    }
+    sc.close();
+  }
+}
