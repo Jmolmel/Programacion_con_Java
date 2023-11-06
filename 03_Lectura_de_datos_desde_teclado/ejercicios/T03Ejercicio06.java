8b9343890cf66ab337d8c793a28611eb55b89803
Jmolmel <josemolinamelendez@gmail.com>
2023-11-06 22:32:12 +0100

    Correccion

--- 03_Lectura_de_datos_desde_teclado/ejercicios/T03Ejercicio06.java
+++ 03_Lectura_de_datos_desde_teclado/ejercicios/T03Ejercicio06.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class T03Ejercicio06 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    System.out.println("Introduzca (separado por un espacio) la base y altura de su triangulo para calcular el area: ");
+    System.out.println("Introduzca (separado por un espacio) la base y altura de su triangulo para calcular el area : ");
     int base = sc.nextInt();
     int altura = sc.nextInt();
 
