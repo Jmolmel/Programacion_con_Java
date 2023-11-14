99c86441a2834991755a211e68070fab26b612c9
Jmolmel <josemolinamelendez@gmail.com>
2023-11-14 16:53:12 +0100

    T04EjercicioWhile

--- 05_Bucles/ejemplos/EjercicioWhile04.java
+++ 05_Bucles/ejemplos/EjercicioWhile04.java
@@ -7,8 +7,9 @@ import java.util.Scanner;
 public class EjercicioWhile04 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int numeroIntroducido = 0 ;   // variable contador que controla salida del bucle
-    int numerosPositivos = 0; // acumulador
+    int numeroIntroducido = 0 ;   // Variable contador que controla salida del bucle
+    int cuentaNumeros = 0; // Variable contadora de numeros positivos introducidos
+    int suma = 0;  // Variable acumuladora de los números positivos introducidos 
 
     System.out.println("Por favor introduzca números enteros positivos: ");
     System.out.println("El programa finalizará cuando se introduzca un número negativo.");
@@ -17,10 +18,12 @@ public class EjercicioWhile04 {
       System.out.print("Número: ");
       numeroIntroducido = sc.nextInt();
       if (numeroIntroducido > 0) {         // con esto controlas la cantidad de veces que entra un positivo para que no cuente los 0
-        numerosPositivos++;
+        cuentaNumeros++;
+        suma += numeroIntroducido;    // suma = suma + numeroIntroducido
       }
     }
     sc.close();
-    System.out.println("La cantidad de números positivos introducidos es " + numerosPositivos);
+    System.out.println("La cantidad de números positivos introducidos es " + cuentaNumeros);
+    System.out.println("La suma total de ellos es " + (suma));
   }
 }
