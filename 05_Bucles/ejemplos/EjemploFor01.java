740f48ebfb406e8a75d7433b9aae1bb261c7175a
Jmolmel <josemolinamelendez@gmail.com>
2023-11-14 15:55:30 +0100

    EjemplosT05Bucles

---
+++ 05_Bucles/ejemplos/EjemploFor01.java
@@ -0,0 +1,13 @@
+/**
+ * Ejemplo 01 mostrar For
+ * 
+ * @author José Molina Meléndez
+ */
+public class EjemploFor01 {
+  public static void main(String[] args) {
+
+    for (int i = 1; i <= 10; i ++){
+      System.out.println(i);
+    }
+  }
+}
---
+++ 05_Bucles/ejemplos/EjemploFor02.java
@@ -0,0 +1,14 @@
+/**
+ * Imprimir tablas de multiplicar del numero 2 , 4 , 6
+ * 
+ * @author José Molina Meléndez
+ */
+public class EjemploFor02 {
+  public static void main(String[] args) {
+    for (int i = 2 ; i <= 6; i= i + 2) {
+      for(int j = 0; j <= 10; j++){
+      System.out.println(i + " * " + j + " = " + (i*j));
+      }
+    }
+  }
+}
