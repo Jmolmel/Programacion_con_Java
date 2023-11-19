91cf13bdaef13829757910efeaa1f52a97e76957
Jmolmel <josemolinamelendez@gmail.com>
2023-11-19 18:23:17 +0100

    T05 Ej20

--- 05_Bucles/ejercicios/T05Ejercicio19.java
+++ 05_Bucles/ejercicios/T05Ejercicio19.java
@@ -2,36 +2,37 @@ import java.util.Scanner;
 
 /**
  * Realiza un programa que pinte una pirámide por pantalla. La altura se debe
- * pedir por teclado. El carácter con el que se pinta la pirámide también se debepedir por teclado.
+ * pedir por teclado. El carácter con el que se pinta la pirámide también se
+ * debepedir por teclado.
  * 
  * @author José Molina Meléndez
  */
 public class T05Ejercicio19 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    String caracter;
-    int altura;
-    int espacios;
-
 
     System.out.print("Introduzca el caracter para pintar su pirámide: ");
-    caracter = sc.nextLine();
+    String caracter = sc.nextLine();
 
     System.out.print("Introduzca la altura de su pirámide: ");
-    altura = sc.nextInt();
+    int alturaIntroducida = sc.nextInt();
 
-    do {
-      for (int i = 1; i < altura - 1; i++) {
-        System.out.println(" ");
-        for (int j = 1; j < altura + 2; j++) {
-          System.out.println(caracter);
-        }
+    int nivel = 1;
+    int linea = 1;
+    int espacio = alturaIntroducida - 1;
 
-        altura--;
+    while (nivel <= alturaIntroducida) {
+      for (int i = 1; i <= espacio; i++) {
+        System.out.print(" ");
       }
-      
-    } while (altura == 0);
+      for (int i = 1; i <= linea; i++) {
+        System.out.print(caracter);
+      }
+      System.out.println();
 
-
+      nivel++;
+      espacio--;
+      linea += 2;
+    }
   }
 }
--- 05_Bucles/ejercicios/T05Ejercicio20.java
+++ 05_Bucles/ejercicios/T05Ejercicio20.java
@@ -1,5 +1,47 @@
+import java.util.Scanner;
+
+/**
+ * Igual que el ejercicio anterior pero esta vez se debe pintar una pirámide
+ * hueca
+ * 
+ * @author José Molina Meléndez
+ */
 public class T05Ejercicio20 {
   public static void main(String[] args) {
-    System.out.println("Base = h * 2 - 1");
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Introduzca el caracter para pintar su pirámide: ");
+    String caracter = sc.nextLine();
+
+    System.out.print("Introduzca la altura de su pirámide: ");
+    int alturaIntroducida = sc.nextInt();
+
+    sc.close();
+
+    int nivel = 1;
+    int espaciosPorDelante = alturaIntroducida - 1;
+    int espaciosPorDentro = 0;
+
+    while (nivel < alturaIntroducida) {
+      for (int i = 1; i <= espaciosPorDelante; i++) {
+        System.out.print(" ");
+      }
+      System.out.print(caracter);
+
+      for (int i = 1; i <= espaciosPorDentro; i++) {
+        System.out.print(" ");
+      }
+      if (nivel > 1) {
+        System.out.print(caracter);
+      }
+      System.out.println();
+      nivel++;
+      espaciosPorDelante--;
+      espaciosPorDentro += 2;
+
+    }
+    for (int i = 1; i < alturaIntroducida * 2; i++) {
+      System.out.print(caracter);
+    }
   }
 }
