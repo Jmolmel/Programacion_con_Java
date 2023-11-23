edb32d0a8d83c52939da2cea123c7fe5ed4705af
Jmolmel <josemolinamelendez@gmail.com>
2023-11-23 23:15:43 +0100

    T05

--- 05_Bucles/ejercicios/T05Ejercicio22.java
+++ 05_Bucles/ejercicios/T05Ejercicio22.java
@@ -1,19 +1,22 @@
-import java.util.Scanner;
-
 /**
  * Muestra por pantalla todos los números primos entre 2 y 100, ambos incluidos.
+ * 
  * @author José Molina Meléndez
  */
 public class T05Ejercicio22 {
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int primo;
+    boolean esPrimo = true;
 
-    for (int i = 2; i <= 100 ; i++) {
-      for (int j = 1; j < i; j++) {
-        if (i % j == 0 && j != i) {
-        System.out.println(i);
+    for (int n = 2; n <= 100; n++) {
+
+      esPrimo = true;
+      for (int i = 2; i < n; i++) {
+        if (n % i == 0) {
+          esPrimo = false;
+        }
       }
+      if (esPrimo) {
+        System.out.println(n + " ");
       }
     }
   }
---
+++ 05_Bucles/ejercicios/T05Ejercicio23.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+/**
+ * Escribe un programa que lea un número n e imprima una pirámide de números
+ * con n filas como en la siguiente figura:
+ * 
+ * @author José Molina Meléndez
+ */
+public class T05Ejercicio23 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduzca un número para crear una pirámide de numeros: ");
+        int numero = sc.nextInt();
+
+        System.out.println("Introduzca cuantas filas quiere en su piramide");
+
+    }
+
+}
