3e1463f75de9facb3fb12acab913ffa7fe03043c
Jmolmel <josemolinamelendez@gmail.com>
2023-12-16 11:25:56 +0100

    T08

--- 08_Funciones/ejemplos/PruebaFunciones.java
+++ 08_Funciones/ejemplos/PruebaFunciones.java
@@ -1,37 +1,50 @@
 import java.util.Scanner;
+
+import matematicas.Area;
 import matematicas.Varias;
 import matematicas.Volumen;
+
 public class PruebaFunciones {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-  int n;
+    int n;
 
-  //Probamos la función esPrimo(), alojada en la clase Varias dentro del paquete matemáticas
+    // Probamos la función esPrimo(), alojada en la clase Varias dentro del paquete
+    // matemáticas
 
-  System.out.println("Introduzca en número");
-  n = sc.nextInt();
+    System.out.println("Introduzca en número");
+    n = sc.nextInt();
 
-  if (Varias.esPrimo(n)) {
-    System.out.println("Su número es primo");
-  } else {
-    System.out.println("Su número no es primo");
+    if (Varias.esPrimo(n)) {
+      System.out.println("Su número es primo");
+    } else {
+      System.out.println("Su número no es primo");
+    }
+
+    System.out.println("Introduzca un dígito: ");
+    n = sc.nextInt();
+    System.out.println();
+    int numDigitos = Varias.digitos(n);
+    System.out.println(n + " tiene " + numDigitos + " digitos.");
+
+    double r;
+    double h;
+    System.out.print("Introduzca el radio: ");
+    r = sc.nextDouble();
+
+    System.out.print("Introduzca la altura: ");
+    h = sc.nextDouble();
+
+    System.out.println("El volumen del cilindro es " + Volumen.volumenCilindro(r, h));
+
+    System.out.println("Introduzca base de su rombo: ");
+    double baseRombo = sc.nextDouble();
+
+    System.out.println("Introduzca altura de su rombo: ");
+    double alturaRombo = sc.nextDouble();
+
+    // Rombo
+    System.out.println("El área de su rombo es: " + Area.calcularAreaRombo(baseRombo, alturaRombo));
+
   }
-
-  System.out.println("Introduzca un dígito: ");
-  n = sc.nextInt();
-  System.out.println();
-  int numDigitos = Varias.digitos(n);
-  System.out.println(n + " tiene " + numDigitos + " digitos.");
-
-  double r;
-  double h;
-  System.out.print("Introduzca el radio: ");
-  r = sc.nextDouble();
-
-  System.out.print("Introduzca la altura: ");
-  h = sc.nextDouble();
-
-  System.out.println("El volumen del cilindro es " + Volumen.volumenCilindro(r, h));
-  
-}
 }
--- 08_Funciones/ejemplos/matematicas/Area.java
+++ 08_Funciones/ejemplos/matematicas/Area.java
@@ -1,5 +1,15 @@
 package matematicas;
 
 public class Area {
+    // Area rombo
+    public static double calcularAreaRombo(double diagonalMayor, double diagonalMenor) {
+        return (diagonalMayor * diagonalMenor) / 2;
+    }
+
+    // Area rectangulo
+
+    public static int calcularAreaRectangulo(int base, int altura) {
+        return (base * altura);
+    }
 
 }
