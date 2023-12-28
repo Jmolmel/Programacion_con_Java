948fd4a4a7cbe9ab7d7537fe60f070be0b84d0ce
Jmolmel <josemolinamelendez@gmail.com>
2023-12-28 18:20:44 +0100

    Bucle

--- 99_Practica/ejercicios/CompruebaMail.java
+++ 99_Practica/ejercicios/CompruebaMail.java
@@ -4,20 +4,19 @@ public class CompruebaMail {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        boolean arroba = false;
-        String mail = "";
+        int arroba = 0;
 
         System.out.println("Introduce tu email");
-        mail = sc.nextLine();
+        String mail = sc.nextLine();
 
         for (int i = 0; i < mail.length(); i++) {
 
             if (mail.charAt(i) == '@') {
-                arroba = true;
+                arroba++;
             }
 
         }
-        if (arroba == true) {
+        if (arroba == 1) {
             System.out.println("Su mail es correcto.");
         } else {
             System.out.println("Su mail no es correcto.");
