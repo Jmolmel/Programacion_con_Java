3551b8dc8ac8a596a8e74b2366b6fad68cc5c683
Jmolmel <josemolinamelendez@gmail.com>
2024-01-20 11:52:21 +0100

    POO

---
+++ 09_POO/ejercicios/Baraja.java
@@ -0,0 +1,13 @@
+public class Baraja {
+  //Atributos de baraja
+  String palos;
+  int valores;
+  int cartas;
+  int numeroDeCartas;
+  //ARRAY DE OBJETOS CARTA
+
+  //Constructor. CREAMOS EL ARRAY DE CARTAS -
+  //INICIALIZAR EL NUMERO DE CARTAS  
+  //INICIALIZAR LA BARAJA
+  //EXTRAER CARTA
+}
