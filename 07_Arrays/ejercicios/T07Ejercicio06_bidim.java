69c85b54f7d3ecccefd0ec45ec6fc78ae8506bfd
Jmolmel <josemolinamelendez@gmail.com>
2024-01-23 17:14:19 +0100

    Arreglar bucle

--- 07_Arrays/ejercicios/T07Ejercicio06_bidim.java
+++ 07_Arrays/ejercicios/T07Ejercicio06_bidim.java
@@ -3,52 +3,49 @@ public class T07Ejercicio06_bidim {
         int[][] numero = new int[6][10];
         int maximo = Integer.MIN_VALUE;
         int minimo = Integer.MAX_VALUE;
-        boolean esRepetido = true;
+
+        
+        int[] numerosGenerados = new int[61];
+
+        
         for (int i = 0; i < numero.length; i++) {
             for (int j = 0; j < numero[i].length; j++) {
+                int numAleatorio;
+                boolean esRepetido;
+
 
                 do {
-                    numero[i][j] = (int) (Math.random() * 1001);
-                    for (int j2 = 0; j2 < numero.length; j2++) {
-                        for (int k = 0; k < numero[j2].length; k++) {
-                            if (numero[i][j] == numero[j2][k]) {
-                                esRepetido = true;
-                            }
-                        }
-                    }
-                    esRepetido = false;
-                } while (!esRepetido(numero, i, j));
+                    numAleatorio = (int) (Math.random() * 61);
+                    esRepetido = numerosGenerados[numAleatorio] == 1;
+                } while (esRepetido);
+
+                numero[i][j] = numAleatorio;
+                numerosGenerados[numAleatorio] = 1; 
+
+
+
                 
                 if (numero[i][j] > maximo) {
                     maximo = numero[i][j];
-                } else if (numero[i][j] < minimo) {
+                }
+
+                if (numero[i][j] < minimo) {
                     minimo = numero[i][j];
                 }
+
                 System.out.printf("%8d", numero[i][j]);
             }
             System.out.println();
         }
+
         for (int i = 0; i < numero.length; i++) {
             for (int j = 0; j < numero[i].length; j++) {
                 if (numero[i][j] == maximo) {
-                    System.out.println(
-                            "El número MAXIMO es: " + numero[i][j] + " en la fila " + (i + 1) + " columna " + (j + 1));
+                    System.out.println("El número MAXIMO es: " + maximo + " en la fila " + (i + 1) + " columna " + (j + 1));
                 } else if (numero[i][j] == minimo) {
-                    System.out.println(
-                            "El número MINIMO es: " + numero[i][j] + " en la fila " + (i + 1) + " columna " + (j + 1));
+                    System.out.println("El número MINIMO es: " + minimo + " en la fila " + (i + 1) + " columna " + (j + 1));
                 }
             }
         }
-    }
-
-    public static boolean esRepetido(int[][] numero, int fila, int columna) {
-        for (int i = 0; i < numero.length; i++) {
-            for (int j = 0; j < numero[i].length; j++) {
-                if (numero[fila][columna] == numero[i][j]) {
-                    return true;
-                }
-            }
-        }
-        return false;
     }
 }
