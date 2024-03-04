e12bf2234b4f93641e3d52eee31ed6b9f66c657c
Jmolmel <josemolinamelendez@gmail.com>
2024-03-04 12:47:10 +0100

    Repaso

--- 07_Arrays/ejercicios/T07Ejercicio13_bidim.java
+++ 07_Arrays/ejercicios/T07Ejercicio13_bidim.java
@@ -2,35 +2,33 @@ public class T07Ejercicio13_bidim {
     public static void main(String[] args) {
         String[] pais = { "España", "Rusia", "Japon", "Australia" };
         int[][] estatura = new int[4][10];
-        int media = 0;
-        int minimo = Integer.MAX_VALUE;
-        int maximo = Integer.MIN_VALUE;
+        // Genero numeros
         for (int i = 0; i < estatura.length; i++) {
             for (int j = 0; j < estatura[i].length; j++) {
-                estatura[i][j] = (int) (Math.random() * 71 + 140);
+                estatura[i][j] = ((int) (Math.random() * 71 + 140));
             }
         }
-        System.out.printf("%78s \n", "MED  MIN  MAX");
+        System.out.printf("%67s \n", "MED  MIN  MAX");
+        // Comprobar min y max
         for (int i = 0; i < estatura.length; i++) {
+            int minimo = Integer.MAX_VALUE;
+            int maximo = Integer.MIN_VALUE;
+            int media = 0;
             System.out.printf("%-12s", pais[i]);
             for (int j = 0; j < estatura[i].length; j++) {
-                System.out.printf("%5d", estatura[i][j]);
-                if (estatura[i][j] < minimo) {
-                    minimo = estatura[i][j];
-                }
+                System.out.printf("%3d ", estatura[i][j]); // Pintar tabla
+                media += estatura[i][j];
                 if (estatura[i][j] > maximo) {
                     maximo = estatura[i][j];
                 }
-                media += estatura[i][j];
+                if (estatura[i][j] < minimo) {
+                    minimo = estatura[i][j];
+                }
             }
-            media = media / 10;
-            System.out.printf("|");
-            System.out.printf("%5d", media);
-            System.out.printf("%5d", minimo);
-            System.out.printf("%5d", maximo);
+            System.out.printf("| %3d ", media / estatura[i].length);
+            System.out.printf("%3d ", minimo);
+            System.out.printf("%3d ", maximo);
             System.out.println();
-            minimo = Integer.MAX_VALUE;
-            maximo = Integer.MIN_VALUE;
         }
     }
 }
